20e30cdd7bc83e2c7364487a41c5140242e86452
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-07-10 11:07:36 +0700

    Update auth

--- apps/auth/src/modules/auth/jwt.strategy.ts
+++ apps/auth/src/modules/auth/jwt.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
-import { JwtDto } from './dto/jwt.dto';
 import { User } from 'src/common/prisma/prisma';
 
 @Injectable()
@@ -18,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: JwtDto): Promise<User> {
+  async validate(payload: any): Promise<User> {
     const user = await this.authService.validateUser(payload.userId);
     if (!user) {
       throw new UnauthorizedException();
